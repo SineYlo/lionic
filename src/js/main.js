9a96afe58dbd8013ff013a81cba36627c4e1f1a8
SineYlo <vavshhik01@mail.ru>
2022-02-11 19:42:15 +1000

    feat: markup has been written for all sections

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,7 @@ import select from './components/home/select';
 import placeholder from './components/home/placeholder';
 // |=============== SCRIPT CONNECTION | LOADERS ===============>
 import loader from './components/home/loader';
+// |=============== SCRIPT CONNECTION | SCROLL SMOOTH ===============>
+import scroll from './components/home/scroll-smooth';
+// |=============== SCRIPT CONNECTION | MAILGO ===============>
+import mail from './components/home/modal-mail';
